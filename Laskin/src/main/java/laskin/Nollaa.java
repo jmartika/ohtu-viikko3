3809eda944e16d5c584bf64bfe6cc053aad3c734
juliamar <julia.martikainen@helsinki.fi>
2018-11-28 15:50:06 +0200

    vko5 teht2 päivitetty

--- Laskin/src/main/java/laskin/Nollaa.java
+++ Laskin/src/main/java/laskin/Nollaa.java
@@ -19,6 +19,7 @@ public class Nollaa implements Komento {
     private TextField syotekentta;
     private Button nollaa;
     private Button undo;
+    private Edelliset edellinen;
 
     public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
         this.sovellus = sovellus;
@@ -29,6 +30,8 @@ public class Nollaa implements Komento {
     }
     @Override
     public void suorita() {
+        
+        this.edellinen = new Edelliset(this.sovellus.tulos());
         this.sovellus.nollaa();
         this.laskunTulos = this.sovellus.tulos();
     
@@ -43,6 +46,15 @@ public class Nollaa implements Komento {
     
      @Override
     public void peru() {
-
+        this.laskunTulos = this.edellinen.getEdellinen();
+        this.sovellus.setTulos(this.laskunTulos);
+        this.syotekentta.setText("");
+        this.tuloskentta.setText("" + this.laskunTulos);
+        if ( this.laskunTulos==0) {
+            this.nollaa.disableProperty().set(true);
+        } else {
+            this.nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(true);
     }
 }
